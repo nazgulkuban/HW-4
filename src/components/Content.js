f73fa1be56d13a290ef56d26222b995e39aaffc5
Nazgul <kubanychbekovan6@gmail.com>
2022-10-15 19:14:46 +0300

    add some project

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+function Content(){
+    return(
+        <div className="content">
+         <h1>Content</h1>
+         <div className="content_cards">
+             <div className="content_info"> 1...</div>
+             <div className="content_info">2...</div>
+             <div className="content_info">3...</div>
+         </div>
+        </div>
+    )
+}
+export default Content
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer(){
+    return(
+        <div className="footer">
+            <h1>Footer</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores
+                libero soluta est necessitatibus porro beatae deserunt consequatur!
+                Ea, ipsam rem omnis veritatis nostrum molestias nemo eaque sit sed ut
+                autem?
+            </p>
+        </div>
+    )
+}
+export default Footer
